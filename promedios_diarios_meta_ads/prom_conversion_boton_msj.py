3b1a070abb2183451e144c9ef74e5bed62ebacca
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-09-04 17:59:40 -0600

    carga de notebook para obtener promedios diarios
    usado en databricks

---
+++ promedios_diarios_meta_ads/prom_conversion_boton_msj.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Cargar el CSV en un DataFrame
+df = pd.read_csv("/dbfs/mnt/processed/anuncios_insights_modificado.csv")
+
+# Asegurarse de que las columnas de fecha están en el formato de fecha adecuado
+df['fecha_inicio'] = pd.to_datetime(df['fecha_inicio'])
+df['fecha_finalizacion'] = pd.to_datetime(df['fecha_finalizacion'])
+
+# Calcular la diferencia de días entre la fecha de inicio y la fecha de finalización
+df['dias_duracion'] = (df['fecha_finalizacion'] - df['fecha_inicio']).dt.days
+
+# Calcular el promedio de conversion al boton de mensaje en el dia (evitando la división por 0)
+df['clicks_por_dia'] = df['conversion_boton_msj'] / df['dias_duracion'].replace(0, 1)
+
+# Agrupar por nombre de anuncio y calcular el promedio de conversion al boton de mensaje
+promedio_clicks_por_dia = df.groupby('nombre_anuncio')['clicks_por_dia'].mean().sort_values(ascending=False)
+
+# Crear el gráfico de barras
+plt.figure(figsize=(10, 6))
+ax = promedio_clicks_por_dia.plot(kind='bar', color='skyblue')
+plt.title('Promedio de conversion al boton de mensaje', fontsize=16)
+plt.xlabel('Nombre del Anuncio', fontsize=14)
+plt.ylabel('Promedio', fontsize=14)
+plt.grid(True, linestyle='--', alpha=0.7)
+
+# Rotar las etiquetas del eje X para mejorar la legibilidad
+ax.set_xticklabels(ax.get_xticklabels(), rotation=90, ha='right')
+
+# Ajustar el diseño para que se vea bien
+plt.tight_layout()
+
+# Mostrar el gráfico
+plt.show()
---
+++ promedios_diarios_meta_ads/prom_costo_por_click_anuncio.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#cargar el csv en un dataframe
+df= pd.read_csv ("/dbfs/mnt/processed/anuncios_insights_modificado.csv")
+
+#asegurar que las columnas fecha este en el formato correcto
+df['fecha_inicio']= pd.to_datetime(df['fecha_inicio'])
+df['fecha_finalizacion'] = pd.to_datetime(df['fecha_finalizacion'])
+
+#calcular la diferencia entre fecha de inicio y finalizacion
+df['dias_duracion']= (df['fecha_finalizacion']-df['fecha_inicio']).dt.days 
+
+#Calcular el promedio de costo por click por dia(evitando dividir entre 0)
+df['clicks_por_dia']= df['click_enlace_trafico']/df['dias_duracion'].replace(0,1)
+
+#Agrupar por nombre de anuncio y calcular el promedio de costo por click
+promedio_clicks_por_dia=df.groupby('nombre_anuncio')['clicks_por_dia'].mean().sort_values(ascending=False)
+
+#crea grafico de barras
+plt.figure(figsize=(10,6))
+ax=promedio_clicks_por_dia.plot(kind='bar', color='red')
+plt.title('Promedio de costo por click en anuncio', fontsize=16)
+plt.xlabel('Nombre del anuncio', fontsize=14)
+plt.ylabel('Promedio', fontsize=14)
+plt.grid(True, linestyle='--', alpha=0.7)
+
+#rotar las etiquetas para mejorar la visualizacion
+ax.set_xticklabels(ax.get_xticklabels(), rotation =90,ha='right')
+
+#ajustar el diseño para que se vea mejor
+plt.tight_layout()
+
+#mostrar
+plt.show()
