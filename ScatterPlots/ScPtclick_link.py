3c84bc28eafda1dcf4e324a2a14e41c0a8ba70f0
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-09-09 17:54:51 -0600

    cambios en carpeta scaterplot

--- ScatterPlots/ScPtclick_link.py
+++ ScatterPlots/ScPtclick_link.py
@@ -1,4 +1,4 @@
-#comparamos la relacion que existe entre conversion_boton_msj con todas las columnas 
+#comparamos la relacion que existe entre click_link con todas las columnas 
 #numericas existentes
 
 import pandas as pd
@@ -11,16 +11,16 @@ df = pd.read_csv("/dbfs/mnt/processed/anuncios_insights_clean05_09_2024.csv")
 # Filtrar solo las columnas numéricas
 numeric_columns = df.select_dtypes(include='number').columns
 
-# Iterar sobre todas las columnas numéricas y crear scatterplots comparándolas con el conversion_boton_msj
+# Iterar sobre todas las columnas numéricas y crear scatterplots comparándolas con el click_link
 for column in numeric_columns:
-    if column != 'conversion_boton_msj':  # Saltar la columna 'conversion_boton_msj' para no compararla consigo misma
+    if column != 'click_link':  # Saltar la columna 'click_link' para no compararla consigo misma
         plt.figure(figsize=(8, 6))
-        sns.scatterplot(x=column, y="conversion_boton_msj", data=df, color="blue")
+        sns.scatterplot(x=column, y="click_link", data=df, color="blue")
         
         # Agregar etiquetas y títulos
-        plt.title(f'Scatterplot de {column} vs conversion_boton_msj')
+        plt.title(f'Scatterplot de {column} vs click_link')
         plt.xlabel(column)
-        plt.ylabel('conversion_boton_msj')
+        plt.ylabel('click_link')
         
         # Mostrar el gráfico
         plt.show()
