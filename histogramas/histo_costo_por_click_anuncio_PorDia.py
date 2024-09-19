8b2aee07e46659edaff96a71d8612676efc65b2c
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-09-19 14:28:01 -0600

    agregue histogramas del csv general por dia
    muestran a lo largo del tiempo como se han movido las variables

---
+++ histogramas/histo_costo_por_click_anuncio_PorDia.py
@@ -0,0 +1,33 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+# Cargar el archivo CSV
+df = pd.read_csv("/dbfs/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado.csv")
+
+# Asegurarse de que la columna date_start esté en formato de fecha
+df['fecha_inicio'] = pd.to_datetime(df['fecha_inicio'])
+
+# Especificar el parámetro que deseas analizar
+parametro_a_analizar = 'costo_por_click_anuncio'  # Cambia este valor por la columna que deseas analizar
+
+# Agrupar por fecha y sumar el parámetro seleccionado
+df_grouped = df.groupby('fecha_inicio')[parametro_a_analizar].sum()
+
+# Crear el histograma para mostrar la distribución del parámetro a lo largo del tiempo (por fecha)
+plt.figure(figsize=(10, 6))
+plt.hist(df_grouped.index, weights=df_grouped.values, bins=30, color='skyblue')
+
+# Formatear las fechas en el eje x para que se muestren con el formato deseado
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%y'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7))  # Intervalo de 7 días
+
+# Añadir etiquetas y título dinámico basado en el parámetro
+plt.title(f'Histograma de {parametro_a_analizar} a lo largo del tiempo (por Fecha)')
+plt.xlabel('Fecha')
+plt.ylabel(f'Cantidad de {parametro_a_analizar}')
+
+# Mostrar el gráfico
+plt.tight_layout()
+plt.show()
--- histogramas/histo_interacciones_Totales_PorDia.py
+++ histogramas/histo_interacciones_Totales_PorDia.py
@@ -9,7 +9,7 @@ df = pd.read_csv("/dbfs/mnt/processed/Ads_General_Por_Dia/anuncios_insights_gene
 df['fecha_inicio'] = pd.to_datetime(df['fecha_inicio'])
 
 # Seleccionar las columnas que especificar para sumar
-columns_to_sum = ['clicks_en_anuncio', 'costo_por_click_anuncio', 'click_enlace_trafico', 'conversion_boton_msj']
+columns_to_sum = ['like', 'reacciones_post', 'comentarios', 'interaccion_post','contenido_guardado','conversion_primer_respuesta','conversion_boton_msj','clicks_en_anuncio','click_link','click_enlace_trafico']
 
 # Agrupar por fecha y sumar las interacciones
 df_grouped = df.groupby('fecha_inicio')[columns_to_sum].sum()
@@ -22,7 +22,7 @@ plt.figure(figsize=(10, 6))
 plt.hist(df_grouped.index, weights=df_grouped['total_interacciones'], bins=30, color='skyblue')
 
 # Formatear las fechas en el eje x
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%y'))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%y/%m/%d'))
 plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=7))
 
 # Añadir etiquetas y título
