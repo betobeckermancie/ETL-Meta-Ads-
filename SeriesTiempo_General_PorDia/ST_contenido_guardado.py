551c3b4394909b920c03659bbd14b29c9038607b
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-09-26 11:51:35 -0600

    se agregaron notebooks faltantes

---
+++ SeriesTiempo_General_PorDia/ST_contenido_guardado.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#cargamos el archivo csv
+df= pd.read_csv("/dbfs/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado.csv")
+
+#confirmar que la columna fecha_inicio sea tipo datetime
+df['fecha_inicio'] = pd.to_datetime(df["fecha_inicio"])
+
+#establecer la columna 'fecha_inicio' como indice para aplicar a la time serie
+df.set_index("fecha_inicio", inplace=True)
+
+#se usa para hacer dinamico el titulo
+variable_analizar ="contenido_guardado" #cambiar cuanndo sea necesario
+
+#seleccionar las columnas para analizar
+df_time_series = df[variable_analizar]
+
+#resamplear los datos segun sea necesario analizar Dia 'D',
+#semana 'W' o año 'Y'
+df_resampled = df_time_series.resample("D").sum()
+
+#se usa para hacer dinamicas las etiquetas
+if df_time_series.resample("D"):
+    fecha="Dia"
+elif df_time_series.resample("W"):
+    fecha="Semana"
+elif df_time_series.resample("M"):
+    fecha="Mes"
+else:
+    fecha="Anual"
+
+#Graficar la serie de tiempo
+plt.figure(figsize=(10, 6))
+df_resampled.plot(title=f"Serie de tiempo de {variable_analizar} por dia", color="blue")
+
+#agregar etiquetas a la grafica
+plt.xlabel(f"Analisis de {fecha}")
+plt.ylabel("Total")
+plt.grid(True)
+
+#mostrar la grafica
+plt.tight_layout()
+plt.show()
