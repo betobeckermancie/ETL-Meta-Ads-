f7a3885c37c00cd60bb0934bdcfbe92955e49e1b
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-10-07 16:49:16 -0600

    load click_link y clicks_anuncio

---
+++ GraficoDensidad_GralPorDia/GrafDensidad_click_link.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#cargar el csv
+df=pd.read_csv("/dbfs/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado.csv")
+
+#variable para analizar
+variable_analizar = 'click_link' #se cambia la variable para analizar
+
+#creo grafica de densidad
+plt.figure(figsize=(10, 6))
+sns.kdeplot(df[variable_analizar],fill=True)
+
+#etiquetas y titulo de la grafica
+plt.title(f"Densidad de {variable_analizar}")
+plt.xlabel("Valor")
+plt.ylabel("Densidad")
+
+#intervalos de 100 en 100
+plt.xticks(range(0, int(df[variable_analizar].max())+ 100, 100))
+plt.tight_layout()
+plt.show()
+
