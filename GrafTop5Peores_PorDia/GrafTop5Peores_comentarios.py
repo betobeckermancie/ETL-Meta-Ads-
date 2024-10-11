b8dd6a439c35df4ba6e5d56e18c67dd6d875deb2
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-10-11 10:59:21 -0600

    Carga notebooks peor numero de variables

---
+++ GrafTop5Peores_PorDia/GrafTop5Peores_comentarios.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Cargamos el csv
+df = pd.read_csv("/dbfs/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado.csv")
+
+# varible para analizar/se cambia 
+variable_analizar = "comentarios"
+
+# se agrupa por el nombre del anuncio para que no se repita
+df_grouped = df.groupby('nombre_anuncio')[variable_analizar].sum()
+
+#ordena la variable y obtiene los 5 con mayor numero
+top_5_ads = df_grouped.nsmallest(5)
+
+# Creamos grafico de barras
+plt.figure(figsize=(10, 6))
+top_5_ads.plot(kind='bar', color="#FF6262")
+
+# etiquetas y titulo a las graficas
+plt.title(f"Top 5 Anuncios con Peor Número de {variable_analizar}", fontsize=16)
+plt.xlabel('Nombre de Anuncio',fontsize=12)
+plt.ylabel(f"{variable_analizar}", fontsize=12)
+plt.xticks(rotation=45, ha="right")
+
+# mostramos el grafico
+plt.tight_layout()
+plt.show()
