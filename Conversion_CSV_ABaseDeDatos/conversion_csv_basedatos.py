d1e280af5c43612191efd5b25719edd2a49fb7bb
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-10-17 15:19:39 -0600

    Update conversion_csv_basedatos.py

--- Conversion_CSV_ABaseDeDatos/conversion_csv_basedatos.py
+++ Conversion_CSV_ABaseDeDatos/conversion_csv_basedatos.py
@@ -1,17 +1,11 @@
-from datetime import datetime
+# creamos la base de datos en caso de que no exista
+spark.sql("CREATE DATABASE IF NOT EXISTS db_pecsa_ads")
 
-#leo archivo csv desde el sistema de archivos de databricks (DFBS)
-df = spark.read.csv("dbfs:/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado.csv",header=True, inferSchema=True)
+# leemos el csv
+df = spark.read.csv("/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado.csv", header=True, inferSchema=True)
 
-#crear una vista temporal para consultar datos usando sql en databricks
-df.createOrReplaceTempView("meta_ads_limpiado_tabla")
+# guardo el dataframe en la base de datos, se sobre escribe en caso de que exista
+df.write.mode("overwrite").saveAsTable("db_pecsa_ads.meta_ads")
 
-
-#obtener fecha actual en formato dd_mm_yyyy
-fecha_actual =datetime.now().strftime("%d_%m_%Y")
-
-#crear el nombre dinamico del archivo o tabla
-nombre_tabla=f"meta_ads{fecha_actual}"
-
-#guardar tabla de forma permanente en databricks para acceder con powerbi
-df.write.format("delta").saveAsTable(nombre_tabla)
+# verifico que la tabla se haya creado
+spark.sql("SHOW TABLES IN db_pecsa_ads").show()
