ef09cafee39765f97a173ba29944f3f988c99d4e
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-10-17 11:25:03 -0600

    carga de notebook para conversion csv a table
    con este uso el archivo csv guardado en el so para luego llamarlo desde databricks azure conectado a powerbi

---
+++ Conversion_CSV_ABaseDeDatos/conversion_csv_basedatos.py
@@ -0,0 +1,15 @@
+#leo archivo csv desde el sistema de archivos de databricks (DFBS)
+df = spark.read.csv("dbfs:/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado.csv",header=True, inferSchema=True)
+
+#crear una vista temporal para consultar datos usando sql en databricks
+df.createOrReplaceTempView("meta_ads_limpiado_tabla")
+
+
+#obtener fecha actual en formato dd_mm_yyyy
+fecha_actual =datetime.now().strftime("%d_%m_%Y")
+
+#crear el nombre dinamico del archivo o tabla
+nombre_tabla=f"meta_ads{fecha_actual}"
+
+#guardar tabla de forma permanente en databricks para acceder con powerbi
+df.write.form("delta").saveAsTable("meta_ads")
