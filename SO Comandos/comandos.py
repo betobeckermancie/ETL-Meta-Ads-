e1262231d6b039e65eb29f9aab78ba7a3dabcf95
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2024-11-12 11:09:46 -0600

    Update comandos.py

--- SO Comandos/comandos.py
+++ SO Comandos/comandos.py
@@ -2,7 +2,7 @@
 dbutils.fs.ls("/mnt/processed")
 
 #borrar archivos expecificos dentro de una carpeta
-dbutils.fs.rm("/mnt/processed/archivo_a_eliminar.csv")
+dbutils.fs.rm("/mnt/processed/archivo_a_eliminar.csv", True)
 
 #cambiar el nombre a un archivo
 dbutils.fs.mv("dbfs:/mnt/processed/anuncios_insights05092024.csv", "dbfs:/mnt/processed/anuncios_insights05_09_2024.csv")
