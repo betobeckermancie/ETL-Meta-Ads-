86871d2f11c5e060294f6fa31749ad2d0cc02b81
Beto Beckerman Cie <94578320+betobeckermancie@users.noreply.github.com>
2025-01-13 12:09:33 -0600

    Update CambiarColumnasADate.py
    mejora para convertir y eliminar en el mismo codigo

--- limpieza_carga/CambiarColumnasADate.py
+++ limpieza_carga/CambiarColumnasADate.py
@@ -1,12 +1,17 @@
+##convertir 'fecha_inicio' a date para eliminar los registros del 2024
 import pandas as pd
-df =pd.read_csv("/dbfs/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado.csv")
 
-#se convierte la columna en tipo de dato datetime pero solo se guarda la fecha
-df['fecha_inicio']= pd.to_datetime(df['fecha_inicio']).dt.date
-df['fecha_finalizacion'] = pd.to_datetime(df["fecha_finalizacion"]).dt.date
+# leer archivo csv
+df = pd.read_csv("/dbfs/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado_date.csv")
 
-#se guarda en un dataframe
-df.to_csv("/dbfs/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado_date.csv", index=False)
+# convertir la columna 'fecha_inicio' a datetime
+df['fecha_inicio'] = pd.to_datetime(df['fecha_inicio'])
 
-#se muestran los cambios
+# filtrar el dataframe para eliminar registros del 2024
+df = df[df['fecha_inicio'].dt.year != 2024]
+
+# guardar los cambios
+df.to_csv("/dbfs/mnt/processed/Ads_General_Por_Dia/anuncios_insights_general_por_dia_limpiado.csv", index=False)
+
+# muestra los cambios
 display(df)
